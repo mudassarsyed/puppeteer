34690f11b25f71b6c88f26db23bde850667f587b
Mudassar Syed <mudassarsyed@Mudassars-MacBook-Pro.local>
2020-05-09 00:07:27 +0530

    first commit1

---
+++ example.js
@@ -0,0 +1,31 @@
+const assert = require('assert')
+const puppeteer = require('puppeteer')
+const { percySnapshot } = require('@percy/puppeteer')
+
+let browser
+let page
+
+before(async () => {
+  browser = await puppeteer.launch()
+  page = await browser.newPage()
+})
+
+describe('Duck Duck Go Search', () => {
+  it('returns Chrome Puppeteer Github repo as first search result"', async () => {
+
+    await page.goto('https://duckduckgo.com/', { waitUntil: 'networkidle2' })
+    await percySnapshot(page,'homepage')
+    await page.type('input#search_form_input_homepage', 'chrome puppeteer', { delay: 50 })
+    await page.click('input#search_button_homepage')
+    await page.waitForSelector('.results--main #r1-0')
+    
+    const githubLink = await page.evaluate(() => document.querySelector('a.result__a').textContent.trim())
+    assert(githubLink, 'https://github.com/GoogleChrome/puppeteer')
+    await page.screenshot({ path: 'duckduckgo.png' })
+    await percySnapshot(page, 'githublink')
+  }).timeout(10000)
+})
+
+after(async () => {
+  await browser.close()
+})
